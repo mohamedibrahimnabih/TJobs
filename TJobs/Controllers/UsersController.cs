ea5717c32c05d2b82aac8817f3ed25945cb6bfd7
mohamedibrahimnabih <eng.mohamedibrahimnabih@gmail.com>
2025-06-17 15:30:10 +0300

    Users Controller

---
+++ TJobs/Controllers/UsersController.cs
@@ -0,0 +1,54 @@
+﻿using Mapster;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace TJobs.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly IEmailSender _emailSender;
+        private readonly ApplicationDbContext _context;
+
+        public UsersController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, IEmailSender emailSender, ApplicationDbContext context)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _emailSender = emailSender;
+            _context = context;
+        }
+
+        [HttpGet("")]
+        public async Task<IActionResult> GetAll()
+        {
+            var users = _userManager.Users.ToList();
+
+            var usersResponse = new List<UserResponse>();
+
+            foreach (var user in users)
+            {
+                var roles = await _userManager.GetRolesAsync(user);
+
+                var userResponse = new UserResponse
+                {
+                    FirstName = user.FirstName,
+                    LastName = user.LastName,
+                    Email = user.Email,
+                    LockoutEnabled = user.LockoutEnabled.ToString(),
+                    Roles = roles.ToList()
+                };
+
+                usersResponse.Add(userResponse);
+            }
+
+            return Ok(usersResponse);
+        }
+
+    }
+}
---
+++ TJobs/DTOs/Responses/UserResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace TJobs.DTOs.Responses
+{
+    public class UserResponse
+    {
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string LockoutEnabled { get; set; } = string.Empty;
+        public List<string> Roles { get; set; } = null!;
+    }
+}

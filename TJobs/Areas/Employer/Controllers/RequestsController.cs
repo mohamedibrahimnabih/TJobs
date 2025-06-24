12f340621e87f8b28ae1c6479a55471c00f43289
mohamedibrahimnabih <eng.mohamedibrahimnabih@gmail.com>
2025-06-24 23:09:44 +0300

    FIX JWT

--- TJobs/Areas/Employer/Controllers/RequestsController.cs
+++ TJobs/Areas/Employer/Controllers/RequestsController.cs
@@ -1,4 +1,5 @@
 ﻿using Mapster;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -11,6 +12,7 @@ namespace TJobs.Areas.Employer.Controllers
     [Route("api/[area]/[controller]")]
     [Area("Employer")]
     [ApiController]
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
     public class RequestsController : ControllerBase
     {
         private readonly ApplicationDbContext _context;
